fe1a442c4ccc6da6a30cd2fe49193596303fb351
EetuK <eetukoskelainen@hotmail.com>
2020-03-19 19:09:13 +0200

    Adding google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,6 +64,20 @@ module.exports = {
         pageContextProperty: `menus`,
         menus: require("./src/data/menus.json")
       }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-131931747-2",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        cookieDomain: "maisahyttinen.fi"
+      }
     }
   ]
 };
