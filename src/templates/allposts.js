d3bdd042fcbbed174b0783c1e06ff16e2dc2698b
EetuK <eetukoskelainen@hotmail.com>
2020-04-23 00:29:06 +0300

    fix

--- src/templates/allposts.js
+++ src/templates/allposts.js
@@ -47,7 +47,7 @@ const Allposts = (props) => {
               const { month, day } = getSeparateDate(post.frontmatter.date);
               return (
                 <React.Fragment key={i}>
-                  <Link to={`../posts/${post.name}`}>
+                  <Link to={`/posts/${post.name}`}>
                     <strong>{`${day}.${month}.`}</strong>
                     {`  ${post.frontmatter.title}`}
                   </Link>
