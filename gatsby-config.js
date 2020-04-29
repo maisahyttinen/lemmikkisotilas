66b648afcf26956f5f05e6ed6abd553238b3cc0b
EetuK <eetukoskelainen@hotmail.com>
2020-04-29 23:35:32 +0300

    change nelifycms congif

--- gatsby-config.js
+++ gatsby-config.js
@@ -148,7 +148,8 @@ module.exports = {
     {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
+        //modulePath: `${__dirname}/src/cms/cms.js`,
+        enableIdentityWidget: false,
       },
     },
     "gatsby-plugin-netlify", // make sure to keep it last in the array
