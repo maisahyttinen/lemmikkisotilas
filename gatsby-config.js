fc60a9598d067f9a9cf3cecee160289cb52568ce
EetuK <eetukoskelainen@hotmail.com>
2020-04-29 21:51:16 +0300

    remove rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -88,63 +88,63 @@ module.exports = {
         username: `maisahyttinen`,
       },
     },
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map((edge) => {
-                const image = edge.node.frontmatter.img_path;
-                return Object.assign({}, edge.node.frontmatter, {
-                  title: edge.node.frontmatter.title,
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
-                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
-                  ...(image && { image }),
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                });
-              });
-            },
-            query: `
-            {
-              allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {fields: {slug: {nin: ["/", "/arkisto/", "/tekija/"]}}}) {
-                edges {
-                  node {
-                    excerpt
-                    html
-                    fields {
-                      slug
-                    }
-                    frontmatter {
-                      title
-                      date
-                      path
-                    }
-                  }
-                }
-              }
-            }
-            `,
-            output: "/rss.xml",
-            title: "Maisa Hyttinen",
-          },
-        ],
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-feed`,
+    //   options: {
+    //     query: `
+    //       {
+    //         site {
+    //           siteMetadata {
+    //             title
+    //             description
+    //             siteUrl
+    //             site_url: siteUrl
+    //           }
+    //         }
+    //       }
+    //     `,
+    //     feeds: [
+    //       {
+    //         serialize: ({ query: { site, allMarkdownRemark } }) => {
+    //           return allMarkdownRemark.edges.map((edge) => {
+    //             const image = edge.node.frontmatter.img_path;
+    //             return Object.assign({}, edge.node.frontmatter, {
+    //               title: edge.node.frontmatter.title,
+    //               description: edge.node.excerpt,
+    //               date: edge.node.frontmatter.date,
+    //               url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+    //               guid: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+    //               ...(image && { image }),
+    //               custom_elements: [{ "content:encoded": edge.node.html }],
+    //             });
+    //           });
+    //         },
+    //         query: `
+    //         {
+    //           allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {fields: {slug: {nin: ["/", "/arkisto/", "/tekija/"]}}}) {
+    //             edges {
+    //               node {
+    //                 excerpt
+    //                 html
+    //                 fields {
+    //                   slug
+    //                 }
+    //                 frontmatter {
+    //                   title
+    //                   date
+    //                   path
+    //                 }
+    //               }
+    //             }
+    //           }
+    //         }
+    //         `,
+    //         output: "/rss.xml",
+    //         title: "Maisa Hyttinen",
+    //       },
+    //     ],
+    //   },
+    // },
     {
       resolve: "gatsby-plugin-netlify-cms",
       options: {
